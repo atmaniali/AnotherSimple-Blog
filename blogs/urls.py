7f9a0b16dfe10f807ba4f7136bbd21c4bcf78cda
atmaniali <atmaniali97@gmail.com>
2021-11-25 00:00:30 +0100

    add forms

--- blogs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HomeView, ArticleDetail
-
-
-urlpatterns = [
-    path('', HomeView.as_view(), name = 'index'), # new
-    path('article/<int:pk>', ArticleDetail.as_view(), name='detail-article')
-]
